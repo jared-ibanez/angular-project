54c1fb1692e91dde33653688a8609a935bc0c9a1
Jared Ibañez <54757792+JaredIbanez69@users.noreply.github.com>
2021-03-03 09:44:44 +0800

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { DisplayComponent } from './display/display.component';
+import { FrameComponent } from './frame/frame.component';
+import { TipsComponent } from './tips/tips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent,
+    DisplayComponent,
+    FrameComponent,
+    TipsComponent
   ],
   imports: [
     BrowserModule,
